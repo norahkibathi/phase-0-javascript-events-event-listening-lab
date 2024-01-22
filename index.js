5334cf145749e3ae1dd8e182786f416d63e9252b
norahkibathi <norah.njeri@student.moringaschool.com>
2024-01-22 00:05:52 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 function addingEventListener() {
+    // the code foes here
+const dodger = document.getElementById("dodger");
+
+function moveDodgerLeft() {
+  // the code moves the DODGER to the left
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+
+function moveDodgerRight() {
+  // the code moves the DODGER to the right
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left <= 360) {
+    dodger.style.left = `${left + 1}px`;
+  }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  } else if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
 }
